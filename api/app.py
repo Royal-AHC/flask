5fd9eb3f3c2f681922910687af48b657200389f5
Royal-AHC <100666593+Royal-AHC@users.noreply.github.com>
2022-03-10 18:08:10 +0530

    Create app.py

---
+++ api/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, send_from_directory, after_this_request, Response, stream_with_context, make_response
+import os
+import sys
+
+root_path = os.path.abspath(__file__)
+root_path = '/'.join(root_path.split('/')[:-2])
+sys.path.append(root_path)
+
+app = Flask(__name__, static_folder="../static",
+            template_folder="../templates")
+
+@app.route('/', methods=['GET'])
+def home():
+    return "Hello"
+
+if __name__ == "__main__":
+    app.run()
